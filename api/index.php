429d2d37201ef1f074d509ae32cc94d4fb1d35f7
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-12-17 23:17:39 +0400

    Fixed json representation

--- api/index.php
+++ api/index.php
@@ -1,3 +1,3 @@
 <?php
     require "../db.php";
-    echo $db->selectData("data");
+    echo ($db->selectData("data"));
--- db.php
+++ db.php
@@ -114,16 +114,21 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
         $sql = "SELECT id, firstname, lastname, avatar FROM $table";
         $result = $conn->query($sql);
-//        if ($result->num_rows > 0) {
-//            // output data of each row
-//            while($row = $result->fetch_assoc()) {
-//                echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//            }
-//        } else {
-//            echo "0 results";
-//        }
+        $data = [];
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $data[] = [
+                    "firstname" => $row["firstname"],
+                    "lastname" => $row["lastname"],
+                    "avatar" => $row["avatar"],
+                ];
+            }
+        } else {
+            echo "0 results";
+        }
         $conn->close();
-        echo json_encode($result->fetch_all());
+        return json_encode($data);
 
     }
 
